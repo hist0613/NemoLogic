b565d69abc91c62c2091d6799df17901a19b28a1
ChoiSeungTaek <hist0134@naver.com>
2016-02-19 22:44:55 +0900

    Add output implementation

--- NemoLogic.cpp
+++ NemoLogic.cpp
@@ -33,3 +33,61 @@ void NemoLogic::input(istream &input){
     board.resize(row_size, vector,int>(col_size));
 }
 
+void NemoLogic::output(ostream &output){
+    int margin_left = 0, max_cnt_row_hints = 0, left_max_digits = 0;
+    int margin_top = 0, max_cnt_col_hints = 0, top_max_digits = 0;
+
+    for (int ri = 0; ri < row_size; ri++){
+        max_cnt_row_hints = max((int)row_hints[ri].size(), max_cnt_row_hints);
+        for (int hi = 0; hi < row_hints[ri].size(); hi++){
+            left_max_digits = max((int)to_string(row_hints[ri][hi]).size(), left_max_digits);
+        }
+    }
+    for (int ci = 0; ci < col_size; ci++){
+        max_cnt_col_hints = max((int)col_hints[ci].size(), max_cnt_col_hints);
+        for (int hi = 0; hi < col_hints[ci].size(); hi++){
+            top_max_digits = max((int)to_string(col_hints[ci][hi]).size(), top_max_digits);
+        }
+    }
+
+    margin_left = (left_max_digits + 1) * max_cnt_row_hints;
+    margin_top = top_max_digits * max_cnt_col_hints;
+
+    // print col_hints
+    for (int hi = 0; hi < max_cnt_col_hints; hi++){
+        cout << setw(margin_left) << ' ';
+        for (int ci = 0; ci < col_size; ci++){
+            cout << setw(top_max_digits + 1);
+            if (hi >= max_cnt_col_hints - col_hints[ci].size()){
+                cout << col_hints[ci][hi - max_cnt_col_hints + col_hints[ci].size()];
+            }
+            else{
+                cout << ' ';
+            }
+        }
+        cout << endl;
+    }
+
+    for (int ri = 0; ri < row_size; ri++){
+        // print row hints
+        for (int hi = 0; hi < max_cnt_row_hints; hi++){
+            cout << setw(left_max_digits + 1);
+            if (hi >= max_cnt_row_hints - row_hints[ri].size()){
+                cout << row_hints[ri][hi - max_cnt_row_hints + row_hints[ri].size()];
+            }
+            else{
+                cout << ' ';
+            }
+        }
+
+        // print board
+        for (int ci = 0; ci < col_size; ci++){
+            cout << setw(top_max_digits + 1);
+            if (board[ri][ci] == 1) cout << "■";
+            else if (board[ri][ci] == -1) cout << 'X';
+            else cout << "□";
+        }
+
+        cout << endl;
+    }
+}
