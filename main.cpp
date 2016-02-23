81da26b21b6d5157eb2209e76378c0d3c582822e
Choi Seungtaek <hist0134@naver.com>
2016-02-24 00:13:24 +0900

    Change command 'cls' to 'clear' for linux setting

--- main.cpp
+++ main.cpp
@@ -9,12 +9,12 @@ using namespace std;
 #include "NemoLogic.h"
 
 int main(void) {
-	for (int ti = 1; ti <= 1; ti++) {
+	for (int ti = 1; ti <= 7; ti++) {
 		NemoLogic nl;
 		ifstream fin("input" + to_string(ti) + ".txt");
 		ofstream fout("output" + to_string(ti) + ".txt");
 		nl.input(fin);
-		nl.solve(0, true);
+		nl.solve(0, false);
 		// nl.output(cout);
 		nl.output(fout);
 	}
