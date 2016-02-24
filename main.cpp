91e80a1c73749a60c8133d1acc44891b7edc18ac
ChoiSeungTaek <hist0134@naver.com>
2016-02-24 17:42:36 +0900

    Change test code

--- main.cpp
+++ main.cpp
@@ -9,13 +9,13 @@ using namespace std;
 #include "NemoLogic.h"
 
 int main(void) {
-	for (int ti = 1; ti <= 7; ti++) {
+	for (int ti = 7; ti <= 7; ti++) {
 		NemoLogic nl;
 		ifstream fin("input" + to_string(ti) + ".txt");
 		ofstream fout("output" + to_string(ti) + ".txt");
 		nl.input(fin);
-		nl.solve(0, false);
-		// nl.output(cout);
+		nl.solve(0, true);
+		nl.output(cout);
 		nl.output(fout);
 	}
 	return 0;
